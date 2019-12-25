6572c0dc87e1d6d24ff0c2e665f3e3236196dfd8
balajinikhil <balajiedward007@gmail.com>
2019-12-25 22:42:26 +0530

    api

--- API/11.GithHub-API/script.js
+++ API/11.GithHub-API/script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 const ipt = document.querySelector(".ipt");
 const holder = document.querySelector(".holder");
-
+const reset = document.querySelector(".res");
 class Git {
   constructor() {
     this.clientID = "d0b88b29bfc7f26ef9ac";
@@ -11,6 +11,15 @@ class Git {
     const url = `https://api.github.com/users/${ipt.value}?client_id='${this.clientID}'&client_secret='${this.clientSecret}'`;
 
     let res = await fetch(url);
+    if (res.status == 404) {
+      holder.innerHTML = "Enter a valid username";
+      setTimeout(() => {
+        holder.innerHTML = "";
+      }, 5000);
+
+      return;
+    }
+
     let respond = await res.json();
 
     return respond;
@@ -20,8 +29,6 @@ class Git {
 class Display {
   constructor() {}
   showData(d) {
-    console.log(d);
-
     let { avatar_url: src, name, public_repos: repos, html_url } = d;
     holder.innerHTML += `<div class="container">
         <img src='${src}' alt='profile'>
@@ -37,7 +44,15 @@ let ajax = new Git();
 let disp = new Display();
 form.addEventListener("submit", e => {
   e.preventDefault();
-
-  ajax.loadData().then(d => disp.showData(d));
+  ajax
+    .loadData()
+    .then(d => disp.showData(d))
+    .catch(err => {
+      console.log(err);
+    });
   ipt.value = "";
+});
+
+reset.addEventListener("click", () => {
+  holder.innerHTML = "";
 });
